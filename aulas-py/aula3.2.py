3019188e5387c71dfd6890e153bce67b199b0ddd
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-17 17:40:29 -0300

    + sobre laçoes de repetição

---
+++ aulas-py/aula3.2.py
@@ -0,0 +1,9 @@
+#LAÇOS DE REPETIÇÂO + LISTAS
+#Problema 1 a N - imprime os  valores de 1 a N
+
+'''
+input numero maximo
+valor inicial = 1
+loop de valor_inicial a numero_maximo
+   print valor
+'''
