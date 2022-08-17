bbe93e7826f5718dd4ba4025757eb4690da4a1e1
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-17 17:28:55 -0300

    commit

---
+++ aulas-py/aula3.py
@@ -0,0 +1,9 @@
+#LAÇOS DE REPETIÇÂO + LISTAS
+#laços de repetição basicamente é a maneira 
+#de executar o mesmo comando várias vezes
+
+for palavra in range(1,5):
+   print('Repetição')
+
+for item in range(1,20):
+    print(item)
