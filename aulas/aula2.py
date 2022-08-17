54ad92236a008132f5089a41d684394e6ea7fd28
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-17 16:42:00 -0300

    Condicionais em py

---
+++ aulas/aula2.py
@@ -0,0 +1,14 @@
+#CONDICIONAIS
+#if, elif e else
+
+'''
+E ae Gabriel, nora dar uma saída hoje?
+Se eu terminar meu trabalho aqui, eu consigo
+'''
+
+trabalho_terminado = False
+if trabalho_terminado == True:      #if = SE
+    print('Opa!Bora dar uma saída')
+else:
+    print('Não posso sair agora!')
+
