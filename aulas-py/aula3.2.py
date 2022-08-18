6c939f50e30831ac3e924958eb30f39394607252
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-18 10:25:12 -0300

    coleções

--- aulas-py/aula3.2.py
+++ aulas-py/aula3.2.py
@@ -7,3 +7,8 @@ valor inicial = 1
 loop de valor_inicial a numero_maximo
    print valor
 '''
+
+valor_maximo = int(input('Digite o valor máximo'))
+valor_inicial = 1
+for numero in range(valor_inicial,valor_maximo + 1):
+   print(numero)
