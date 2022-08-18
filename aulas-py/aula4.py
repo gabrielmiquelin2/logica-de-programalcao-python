6e8f3e0829b36ae3fd306b144a42a6d2cdae6f17
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-18 10:35:04 -0300

    Create aula4.py

---
+++ aulas-py/aula4.py
@@ -0,0 +1,20 @@
+#Coleções(LISTAS)
+preco_1 = 20
+preco_2 = 50
+preco_3 = 200
+
+#LISTA
+precos = [20,50,200]
+#          0, 1, 2
+print(preco[0])
+print(preco.index(200))
+
+#Lista
+diversidade = [15, 'Gabriel',True]
+print(diversidade[0])
+print(diversidade[1])
+print(diversidade[2])
+
+#Laços em iteráveis
+for preco in precos:
+    print(preco)
