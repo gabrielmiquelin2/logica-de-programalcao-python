dc598919e675aeb8ce9a8c88922b83f4341873fa
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-18 10:41:16 -0300

    coleçõs usando listas

---
+++ aulas-py/aula4.2.py
@@ -0,0 +1,21 @@
+#coleções(LISTAS)
+'''
+Exemplo 5 Some os valores
+Dados uma coleção de dados"idades"[15,46,75,34,23],
+imprima na telaasoma deste valores
+
+ALGORITMO USANDO PSEUDOCODIGO
+
+idades = [15,46,75,34,23]
+total = 0
+loop idade em idades
+   total = total + idade
+print total   
+
+'''
+#ALGORITMO EM PYTHON
+idades = [15,46,75,34,23]
+total = 0
+for idade in idades:
+    total = total + idade
+print(total)    
